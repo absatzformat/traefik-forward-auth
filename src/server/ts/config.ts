79f62679c66b0bb6daa22033821e878e484ea25d
Johannes Rabausch <mail@jrabausch.de>
2022-07-19 20:08:56 +0000

    Restructuring files

--- src/server/ts/config.ts
+++ src/server/ts/config.ts
@@ -1,3 +1,11 @@
+export type Config = {
+	authFile: string;
+	sendFile: string;
+	serverPort: number;
+	cookieName: string;
+	cookieSecure: boolean;
+};
+
 export default <Config>{
 	authFile: process.env.AUTH_FILE || __dirname + '/secure.json',
 	sendFile: process.env.SEND_FILE || __dirname + '/login.html',
